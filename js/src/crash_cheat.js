8748acbf95c32270b60915ae7dc422aaeb1d535a
HuangQingyun <2409558892@qq.com>
2025-09-29 14:05:48 +0800

    Site updated: 2025-09-29 14:05:48

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -5,12 +5,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images/favicon_1.ico");
         document.title = '╭(°A°`)╮ 你给我回来！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 同济呀同济～' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
